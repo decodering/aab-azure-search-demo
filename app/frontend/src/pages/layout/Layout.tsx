03a3fdb351b69704d6cc8bedb60860cd64fa047c
decodering <20130935+decodering@users.noreply.github.com>
2024-02-01 14:05:58 +0100

    initial commit dump

---
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,29 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+import { useLogin } from "../../authConfig";
+
+import { LoginButton } from "../../components/LoginButton";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>GPT + RAN publications</h3>
+                    </Link>
+                    <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
+                    {useLogin && <LoginButton />}
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
