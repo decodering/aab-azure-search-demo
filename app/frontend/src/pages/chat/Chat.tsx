ac7ea278f1e8ee2c58c19837818a1abd5df562aa
pimscheele <46887467+pimscheele@users.noreply.github.com>
2024-03-12 09:31:55 +0000

    feedbecky big demo update

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -262,7 +262,7 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat about RAN publications</h1>
+                            <h1 className={styles.chatEmptyStateTitle}>Feedbecky</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
                         </div>
@@ -329,7 +329,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. do the ABN AMRO customers like the app?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
